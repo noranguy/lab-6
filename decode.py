6555ca325858770781d700b948b3780a4aa0bbc5
ava-louise2014 <heflina2014@gmail.com>
2023-10-31 18:10:06 -0400

    this is the finished lab and my contribution

--- decode.py
+++ decode.py
@@ -1,4 +1,4 @@
-#Nora Nguyen
+#Nora Nguyen and Ava Heflin
 def encode(string):
     decoded = []
     for i in string:
@@ -7,17 +7,31 @@ def encode(string):
         decoded.append(str(x))
         full_decoded = ''.join(decoded)
     print("Your password has been encoded and stored!\n")
-def main():
-    while True:
-        print("Menu")
-        print("-"*13)
-        print("1. Encode\n2. Decode\n3. Quit\n")
-        user_option = int(input("Please enter an option: "))
-        if user_option == 1:
-            string_encode = input("Please enter your password to encode: ")
-            encode(string_encode)
-        elif user_option == 3:
-            break
+def decode(string):
+    encoded = []
+    for j in string:
+        x = int(j)
+        x += 3
+        encoded.append(str(x))
+        full_encoded = ''.join(encoded)
+        return full_encoded
+
 
 if __name__ == "__main__":
-    main()
+
+        while True:
+            print("Menu")
+            print("-"*13)
+            print("1. Encode\n2. Decode\n3. Quit\n")
+            user_option = int(input("Please enter an option: "))
+            if user_option == 1:
+                string_encode = input("Please enter your password to encode: ")
+                encode(string_encode)
+
+            if user_option == 2:
+                string_decode = decode(string_encode)
+                print("The econded password is", string_decode, ", and the orginal password is", string_encode)
+
+            if user_option == 3:
+                break
+
