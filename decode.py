f10ff390b04b580b9e484ad51cefdd0981291ee9
Nora Nguyen <nora.nguyen@ufl.edu>
2023-10-31 22:02:44 -0400

    final push for version control lab nora nguyen

--- decode.py
+++ decode.py
@@ -1,37 +1,33 @@
 #Nora Nguyen and Ava Heflin
 def encode(string):
-    decoded = []
+    encoded = []
     for i in string:
         x = int(i)
-        x += 3
-        decoded.append(str(x))
-        full_decoded = ''.join(decoded)
-    print("Your password has been encoded and stored!\n")
-def decode(string):
-    encoded = []
-    for j in string:
-        x = int(j)
-        x += 3
+        x = (x+3)%10
         encoded.append(str(x))
         full_encoded = ''.join(encoded)
-        return full_encoded
-
-
+    print("Your password has been encoded and stored!\n")
+    return full_encoded
+def decode(string):
+    decoded = []
+    for j in string:
+        y = int(j)
+        y = (y-3)%10
+        decoded.append(str(y))
+        full_decoded = ''.join(decoded)
+    print(f"The encoded password is {string}, and the original password is {full_decoded}.\n")
 if __name__ == "__main__":
-
         while True:
             print("Menu")
             print("-"*13)
             print("1. Encode\n2. Decode\n3. Quit\n")
-            user_option = int(input("Please enter an option: "))
-            if user_option == 1:
+            user_option = input("Please enter an option: ")
+            if user_option == "1":
                 string_encode = input("Please enter your password to encode: ")
-                encode(string_encode)
-
-            if user_option == 2:
-                string_decode = decode(string_encode)
-                print("The econded password is", string_decode, ", and the orginal password is", string_encode)
-
-            if user_option == 3:
+                encoded_str = encode(string_encode)
+            elif user_option == '2':
+                decode(encoded_str)
+            elif user_option == '3':
                 break
+
 
